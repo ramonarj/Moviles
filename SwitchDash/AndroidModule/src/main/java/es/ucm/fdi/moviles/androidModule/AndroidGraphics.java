977db4f846c53f295bfd84f9eeda299c859cfd8d
Mario de los Santos Sainz <mdelos02@ucm.es>
2019-10-28 21:52:01 +0100

    Implementados metodos de AndroidImage

--- SwitchDash/AndroidModule/src/main/java/es/ucm/fdi/moviles/androidModule/AndroidGraphics.java
+++ SwitchDash/AndroidModule/src/main/java/es/ucm/fdi/moviles/androidModule/AndroidGraphics.java
@@ -6,7 +6,12 @@ import es.ucm.fdi.moviles.interfacemodule.Image;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 import android.util.Log;
+import android.view.SurfaceView;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,10 +21,12 @@ public class AndroidGraphics implements Graphics
 {
     //Actividad de Android
     AppCompatActivity activity;
+    SurfaceView surfaceview_;
 
-    public AndroidGraphics(AppCompatActivity activity)
+    public AndroidGraphics(AppCompatActivity activity, SurfaceView surfaceview_)
     {
         this.activity = activity;
+        this.surfaceview_=surfaceview_;
     }
 
     //Laod image from file
@@ -48,28 +55,50 @@ public class AndroidGraphics implements Graphics
 
     @Override
     public void clear(int color) {
+        //Bloqueamos el canavas para limpiar la pantalla e inmediatamente
+        //despues lo liberamos
+        Canvas canvas=surfaceview_.getHolder().lockCanvas();
+        canvas.drawColor(color);
+        surfaceview_.getHolder().unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void drawImage(Image image,int posX, int posY) {
+        //Bloqueamos el canavas para pintar la imagen e inmediatamente
+        //despues lo liberamos
+        Canvas canvas=surfaceview_.getHolder().lockCanvas();
+        if(image!=null) {
+            AndroidImage im=(AndroidImage)image;
+            canvas.drawBitmap(im.sprite,posX,posY,null);
+        }
+        surfaceview_.getHolder().unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public void drawImage(Image image, int posX, int posY, float alpha) {
+        //Bloqueamos el canavas para pintar la imagen e inmediatamente
+        //despues lo liberamos,creamos un Paint local el cual usaremos
+        //para modificar el alpha de la imagen
+        Canvas canvas=surfaceview_.getHolder().lockCanvas();
+        if(image!=null) {
+            AndroidImage im=(AndroidImage)image;
+            Paint paint=new Paint();
+            paint.setAlpha((int)alpha*10);
+            canvas.drawBitmap(im.sprite,posX,posY,paint);
+        }
+    }
+
+    @Override
+    public void drawImage(Image image, int posX, int posY, float alpha, float scaleX, float scaleY) {
 
     }
 
     @Override
-    public void drawImage(Image image) {
+    public void drawImage(Image image, int posX, int posY, float alpha, float scaleX, float scaleY, int rectMin, int rectMax) {
 
     }
 
-    @Override
-    public void drawImage(Image image, float alpha) {
 
-    }
-
-    @Override
-    public void drawImage(Image image, float alpha, float scaleX, float scaleY) {
-
-    }
-
-    @Override
-    public void drawImage(Image image, float alpha, float scaleX, float scaleY, float rectMin, float rectMax) {
-
-    }
 
     @Override
     public int getWidth() {
