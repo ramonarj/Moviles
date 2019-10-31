b2d62bd17abe672fa6df93f2ad983d2b4d51bd58
Ramón <ramonarj@ucm.es>
2019-10-31 19:41:20 +0100

    Input

--- SwitchDash/Engine/src/main/java/es/ucm/fdi/moviles/engine/Input.java
+++ SwitchDash/Engine/src/main/java/es/ucm/fdi/moviles/engine/Input.java
@@ -1,6 +1,31 @@
 package es.ucm.fdi.moviles.engine;
 
+import java.util.List;
+
+/**
+ * Gestiona el input
+ */
 public interface Input
 {
+    /**
+     * Representa la información de un toque sobre la pantalla
+     */
+    public static class TouchEvent
+    {
+        //Tipo enumerado para el tipo de evento
+        public enum EventType {PRESSED, RELEASED, MOVED}
 
+        //Tipo (pulsacion, liberacion, desplazamiento)
+        EventType type;
+
+        //Posicion
+        int x;
+        int y;
+
+        //Identificador del dedo/boton
+        int id;
+    }
+
+    //Lista de eventos
+    List<TouchEvent> getTouchEvents();
 }
