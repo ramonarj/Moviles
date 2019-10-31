c0281a88bffecb9af7b83afe108b7d6ac594edbc
Ramón <ramonarj@ucm.es>
2019-10-31 19:40:22 +0100

    PCInput

---
+++ SwitchDash/PCModule/src/main/java/es/ucm/fdi/moviles/pcmodule/PCInput.java
@@ -0,0 +1,16 @@
+package es.ucm.fdi.moviles.pcmodule;
+
+import java.util.List;
+
+import es.ucm.fdi.moviles.engine.Input;
+
+/**
+ * Implementa la clase Input para PC
+ */
+public class PCInput implements Input {
+
+    @Override
+    public List<TouchEvent> getTouchEvents() {
+        return null;
+    }
+}
