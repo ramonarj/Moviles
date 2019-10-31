dcaaf7e7fa6f39c212f0826b37c846cdd0d98c4d
Ramón <ramonarj@ucm.es>
2019-10-31 19:39:44 +0100

    AndroidInput

--- SwitchDash/AndroidModule/src/main/java/es/ucm/fdi/moviles/androidModule/AndroidInput.java
+++ SwitchDash/AndroidModule/src/main/java/es/ucm/fdi/moviles/androidModule/AndroidInput.java
@@ -1,7 +1,18 @@
 package es.ucm.fdi.moviles.androidModule;
 
+import java.util.List;
+
 import es.ucm.fdi.moviles.engine.Input;
 
+/**
+ * Implementa la clase Input para Android
+ */
 public class AndroidInput implements Input {
+    //Lista de eventos recibidos
+    List<TouchEvent> events = null;
 
+    @Override
+    public List<TouchEvent> getTouchEvents() {
+        return null;
+    }
 }
