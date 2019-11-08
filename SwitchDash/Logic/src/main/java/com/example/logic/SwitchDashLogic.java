644b7f2fbf1647ee8a3eb71822bdd0e9e5db59df
Ramón <ramonarj@ucm.es>
2019-11-08 21:49:56 +0100

    Ejemplo de render en PC

--- SwitchDash/Logic/src/main/java/com/example/logic/SwitchDashLogic.java
+++ SwitchDash/Logic/src/main/java/com/example/logic/SwitchDashLogic.java
@@ -1,30 +1,48 @@
 package com.example.logic;
 
+import java.util.ArrayList;
+
 import es.ucm.fdi.moviles.engine.Game;
 import es.ucm.fdi.moviles.engine.GameState;
 import es.ucm.fdi.moviles.engine.Graphics;
+import es.ucm.fdi.moviles.engine.Image;
 import es.ucm.fdi.moviles.engine.Input;
 
 public class SwitchDashLogic implements GameState
 {
+    //Objeto del juego
     private Game game;
+
+    //Imágenes
+    Image prueba;
     public SwitchDashLogic(Game game)
     {
         this.game = game;
     }
 
     @Override
+    public boolean init()
+    {
+        prueba = game.getGraphics().newImage("assets/Chestplate.jpg");
+        return true;
+    }
+
+    @Override
     public void update(float deltaTime)
     {
         Input input = game.getInput();
+        ArrayList<Input.TouchEvent> events = (ArrayList)input.getTouchEvents();
+        for(Input.TouchEvent evt: events)
+        {
+            System.out.println("X:" + evt.x + ", Y: " + evt.y);
+        }
     }
 
     @Override
     public void render()
     {
-        //TODO: hay que gestionar errores con el strategy.contentsLost()
         Graphics g = game.getGraphics();
-        g.clear(0x0000FF00);
-
+        g.drawImage(prueba, 0, 0);
+        //g.clear(0x0000FF00);
     }
 }
