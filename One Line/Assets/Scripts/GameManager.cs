84a1f787a9138a9dee6da4f5a1fe404351b21a15
Ramón <ramonarj@ucm.es>
2019-12-12 19:12:37 +0100

    Escena del nivel

---
+++ One Line/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        // Singleton
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+}
